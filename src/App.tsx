fd875789ddeb483d01aead42e8b643886f9bf520
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-12 17:19:53 +0530

    added login, signup and home routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./components";
+import { Login, Signup, Home } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <p>Chat App</p>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path={"/"}
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 }
