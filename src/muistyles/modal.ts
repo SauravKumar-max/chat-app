490c5fb16761d0225acc60e86d692d3d81f2c81e
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-12 16:45:07 +0530

    styles: mui styles for modals

---
+++ src/muistyles/modal.ts
@@ -0,0 +1,31 @@
+export const modalContainer = {
+    position: "absolute" as "absolute",
+    top: "45%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: { xs: 350, md: 400 },
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 3,
+    borderRadius: 1,
+}
+
+export const modalHeader = {
+    color: "text.secondary",
+    fontSize: "1.3rem",
+    fontWeight: "bold",
+    mb: 2,
+}
+
+export const textField = {
+    width: "100%",
+    background: "#dcdce8a6",
+}
+
+export const emptySearchText = {
+    fontSize: "1.8rem",
+    fontWeight: "bold",
+    color: "GrayText",
+    m: 2,
+    textAlign: "center",
+}
