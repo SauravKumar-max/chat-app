cbc6e2583d992406aac21801d4fcdb0c84b45f58
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-12 16:44:42 +0530

    styles: mui styles for login and signup form

---
+++ src/muistyles/auth.ts
@@ -0,0 +1,44 @@
+export const buttonStyle = {
+    m: 1,
+    p: 1,
+    width: "100%",
+    fontWeight: "bold",
+    fontSize: "1rem",
+}
+
+export const disabledButtonStyle = {
+    m: 1,
+    p: 1,
+    width: "100%",
+    fontWeight: "bold",
+    fontSize: "1rem",
+    opacity: 0.7,
+}
+
+export const authContainer = {
+    margin: "10rem auto 2rem auto",
+    width: "90%",
+    maxWidth: "28rem",
+    border: "solid 1px #adadad",
+    p: { xs: 2, sm: 3 },
+    boxShadow: 2,
+    borderRadius: 1,
+}
+
+export const authHeader = {
+    align: "center",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    color: "text.primary",
+}
+
+export const authError = {
+    width: "100%",
+    fontWeight: "bold",
+    fontSize: "0.9rem",
+    color: "white",
+    background: "red",
+    align: "center",
+    borderRadius: 2,
+    p: 1,
+}
